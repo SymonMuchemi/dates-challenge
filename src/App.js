cdcff43e440d0558a26c733fcbc45f5c23fb588a
SymonMuchemi <simonkenya846@gmail.com>
2023-10-09 02:52:08 +0300

    Logic: done!

--- src/App.js
+++ src/App.js
@@ -1,40 +1,48 @@
 import { useState } from "react";
 import Button from "./buttons";
 
+const getCurrentDate = (days) => {
+  let date =new Date(2027, 5, days);
+
+  return date.toDateString();
+}
+
 function App() {
 
   const [stepCount, setStepCount] = useState(0);
   const [count, setCount] = useState(0);
+  
 
-  let date = new Date();
-  let displayDate = date.toDateString();
-  console.log(displayDate);
 
   return (
     <>
     <div>
       <h1>Date Counter Challenge</h1>
-      <Button type={'reduce'}
-              count={stepCount}
-              setCount={setStepCount} />
-      Step: {stepCount}
-      <Button type={'add'}
-              count={stepCount}
-              setCount={setStepCount} />
+        <Button type={'reduce'}
+                count={stepCount}
+                setCount={setStepCount} />
+            <span>Step: {stepCount}</span>
+        <Button type={'add'}
+                count={stepCount}
+                setCount={setStepCount} />      
     </div>
     <div>
     <Button type={'reduce'}
               count={count}
               setCount={setCount}
               steps={stepCount} />
-      Count: {count}
+            <span>Count: {count}</span>
       <Button type={'add'}
               count={count}
               setCount={setCount}
               steps={stepCount} />
     </div>
     <div>
-      {count === 0 ? "Today is" : `${count} days from today is `} {`${displayDate}`}
+      <h3>
+        {
+          count === 0 ? `Today is ${getCurrentDate(21)} `
+        : `${count} days from today is ${getCurrentDate(count + 21)}`}
+      </h3>
     </div>
     </>
   );
